37e0d8f1890b99455ba1b2a987a231498922c1a7
Karlge <karlgeorgsen@gmail.com>
2020-11-29 23:58:04 +0100

    Added initial code to allow dragging editor boxes around

--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useRef } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.bubble.css';
+import { useState } from 'react';
+import styled from 'styled-components';
 const editorOptions = {
   readOnly: false,
   // bounds: '.' + gridBem.block(),
-  theme: 'bubble',
+  theme: 'snow',
   modules: {
     toolbar: [
       [
@@ -39,15 +41,58 @@ const editorOptions = {
     'clean',
   ],
 };
+
+const EditorWrapper = styled.div.attrs((props) => ({
+  style: {
+    left: props.positionOfset.x,
+    top: props.positionOfset.y,
+  },
+}))`
+  position: absolute;
+  &:hover {
+    .header {
+      background: red;
+    }
+  }
+  .header {
+    height: 10px;
+  }
+`;
+
+class ElementPosition {
+  x;
+  y;
+  constructor(x, y) {
+    this.x = this.min(x);
+    this.y = this.min(y);
+  }
+
+  add(xOfset, yOfset) {
+    this.x = this.min(this.x + xOfset);
+    this.y = this.min(this.y + yOfset);
+  }
+
+  min(value, minValue = 0) {
+    return value < minValue ? minValue : value;
+  }
+}
+
 export default function Editor() {
   const editorRef = useRef();
+  const [editor, setEditor] = useState(null);
 
-  const onChange = (value, delta, source) => {
+  const position = useRef(new ElementPosition(100, 100));
+  const lastPosition = useRef(position.current);
+  const [currentPosition, setCurrentPosition] = useState(position.current);
+
+  const onChange = (delta, value, source) => {
     /* source === api | user */
     if (source === 'api') {
       return;
     }
-    console.log(value);
+    console.log(value, source);
+    console.log(delta);
+
     // this.html = this.editor.root.innerHTML;
 
     // if (this.onChangeTimeout) {
@@ -58,17 +103,47 @@ export default function Editor() {
     // this.onChangeTimeout = setTimeout(() => this.commitValue(), 300);
   };
 
+  function startDragging(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    lastPosition.current = new ElementPosition(e.clientX, e.clientY);
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    const { clientX, clientY } = e;
+    const { x: lastX, y: lastY } = lastPosition.current;
+    position.current.add(clientX - lastX, clientY - lastY);
+    lastPosition.current = new ElementPosition(clientX, clientY);
+    setCurrentPosition(
+      new ElementPosition(position.current.x, position.current.y)
+    );
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+
   useEffect(() => {
     const editor = new Quill(editorRef.current, editorOptions);
     console.log(editor);
     editor.on('text-change', onChange);
+    setEditor(editor);
     return () => {};
   }, []);
 
   return (
-    <div>
-      <h1>Editor</h1>
+    <EditorWrapper positionOfset={currentPosition}>
+      <div>{`Current: ${currentPosition.x}, ${currentPosition.y}`}</div>
+      <div className="header" onMouseDown={startDragging} />
       <div ref={editorRef} />
-    </div>
+    </EditorWrapper>
   );
 }
