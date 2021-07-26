ffbd06e3cac8972e8e0a49571e460ff9dfc5fffd
Karlge <karlgeorgsen@gmail.com>
2021-07-26 15:32:37 +0200

    Added functions for getting and saving documents in frontend

--- src/helpers/ipcRenderer.ts
+++ src/helpers/ipcRenderer.ts
@@ -1,13 +1,35 @@
 import { ipcRenderer } from 'electron';
+import EditorDocument from 'Models/EditorDocument';
 import ipcMessages from './ipcMessages';
 
 export class IPCRender {
-  public static async getAllDocuments() {
-    console.log('Sending');
-
-    ipcRenderer.once(ipcMessages.getAllReply, (event, arg) => {
-      console.log(arg); // prints "pong"
+  static async sendMessage(messageName, responseName, value): Promise<any> {
+    return new Promise((resolve) => {
+      ipcRenderer.once(responseName, (event, arg) => {
+        resolve(arg);
+      });
+      ipcRenderer.send(messageName, value);
     });
-    ipcRenderer.send(ipcMessages.getAllMessage, 'ping');
+  }
+  public static async getDocument(uuid: string): Promise<EditorDocument> {
+    return this.sendMessage(
+      ipcMessages.getDocumentMessage,
+      ipcMessages.getDocumentReply,
+      uuid
+    );
+  }
+  public static async getAllDocuments(): Promise<EditorDocument[]> {
+    return this.sendMessage(
+      ipcMessages.getAllMessage,
+      ipcMessages.getAllReply,
+      null
+    );
+  }
+  public static async saveDocument(document: EditorDocument) {
+    return this.sendMessage(
+      ipcMessages.saveDocumentMessage,
+      ipcMessages.saveDocumentReply,
+      document
+    );
   }
 }
