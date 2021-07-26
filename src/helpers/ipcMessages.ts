9da855834e99ffee383e24840b0b1f5d08d46182
Karlge <karlgeorgsen@gmail.com>
2021-07-26 15:32:01 +0200

    Added store encoding for handling data easier Added  functions to fetch and save documents Added dummy data for when database is empty Added messages for getting and saving documents

--- src/helpers/ipcMessages.ts
+++ src/helpers/ipcMessages.ts
@@ -1,4 +1,8 @@
 export default {
+  getDocumentMessage: 'get-document',
+  getDocumentReply: 'get-document-reply',
   getAllMessage: 'get-all',
   getAllReply: 'get-all-reply',
+  saveDocumentMessage: 'save-document',
+  saveDocumentReply: 'save-document-reply',
 };
--- src/main-process/store/store.ts
+++ src/main-process/store/store.ts
@@ -1,28 +1,68 @@
-import ipcMessages from 'Helpers/ipcMessages';
 const { ipcMain } = require('electron');
 var levelup = require('levelup');
 var leveldown = require('leveldown');
+var encode = require('encoding-down');
+import EditorDocument from 'Models/EditorDocument';
+import ipcMessages from 'Helpers/ipcMessages';
+import DocumentMetadata from 'Models/DocumentMetadata';
 
-export function setupStore() {
-  var db = levelup(leveldown('./documents-db'));
-  console.log("Testing testing 123");
-  
-  console.log("Setting up DB");
-  
-  ipcMain.on(ipcMessages.getAllMessage, (event, arg) => {
-    console.log(arg); // prints "ping"
-    event.reply(ipcMessages.getAllReply, 'pong');
+export async function setupStore() {
+  var db = levelup(
+    encode(leveldown('./documents-db'), { valueEncoding: 'json' })
+  );
+
+  console.log('Setting up DB');
+
+  ipcMain.on(ipcMessages.getAllMessage, async (event, arg) => {
+    const docs = await getAllDocuments(db);
+    event.reply(ipcMessages.getAllReply, docs);
   });
-  //   // 2) Put a key & value
-  //   db.put('name', 'levelup', function (err) {
-  //     if (err) return console.log('Ooops!', err); // some kind of I/O error
 
-  //     // 3) Fetch by key
-  //     db.get('name', function (err, value) {
-  //       if (err) return console.log('Ooops!', err); // likely the key was not found
+  ipcMain.on(ipcMessages.saveDocumentMessage, (event, arg) => {
+    console.log(arg);
+    event.reply(ipcMessages.saveDocumentReply, 'Saved!');
+  });
 
-  //       // Ta da!
-  //       console.log('name=' + value);
-  //     });
-  //   });
+  ipcMain.on(ipcMessages.getDocumentMessage, async (event, arg) => {
+    const document = await getDocument(db, arg);
+    event.reply(ipcMessages.getDocumentReply, document);
+  });
+}
+
+async function getDocument(db, uuid: string) {
+  return new Promise((resolve) => {
+    db.get(uuid, (err, value) => {
+      resolve(value);
+    });
+  });
+}
+
+async function getAllDocuments(db) {
+  return new Promise((resolve) => {
+    var docs = [];
+    db.createValueStream()
+      .on('data', (data) => {
+        docs.push(data);
+      })
+      .on('end', () => {
+        if (docs.length > 0) {
+          resolve(docs);
+        } else {
+          const dummyDocs = [
+            new EditorDocument(new DocumentMetadata('Test 1')),
+            new EditorDocument(new DocumentMetadata('Test 2')),
+            new EditorDocument(new DocumentMetadata('Test 3')),
+            new EditorDocument(new DocumentMetadata('Test 4')),
+          ];
+          dummyDocs[2].metaData.parentId = dummyDocs[0].metaData.uuid;
+          db.batch()
+            .put(dummyDocs[0].metaData.uuid, dummyDocs[0])
+            .put(dummyDocs[1].metaData.uuid, dummyDocs[1])
+            .put(dummyDocs[2].metaData.uuid, dummyDocs[2])
+            .put(dummyDocs[3].metaData.uuid, dummyDocs[3])
+            .write(() => resolve('Added dummy data'));
+          resolve(dummyDocs);
+        }
+      });
+  });
 }
