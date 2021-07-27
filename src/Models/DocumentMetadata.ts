3e2e1305b5a8867053d2794327cdee2fb8b015a9
Karlge <karlgeorgsen@gmail.com>
2021-07-27 14:34:30 +0200

    Added an order number to DocumentMetadata

--- src/Models/DocumentMetadata.ts
+++ src/Models/DocumentMetadata.ts
@@ -4,6 +4,7 @@ export default class DocumentMetadata {
   uuid: string;
   title: string;
   parentId: string;
+  order: number;
 
   constructor(title: string = 'New Document', uuid: string = null, parentId: string = undefined) {
     this.title = title;
