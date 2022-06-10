192dcc2a6dcd23db4490e439cf08c6f16dc91486
Karl Gustav Georgsen <karl.georgsen@boligmappa.no>
2022-06-10 20:11:55 +0200

    Made the mouse position a little more pleasant

--- src/Components/ApplicationWrapper/MousePosition.tsx
+++ src/Components/ApplicationWrapper/MousePosition.tsx
@@ -4,11 +4,13 @@ import styled from 'styled-components';
 
 const MousePositionIndicator = styled.div`
   position: absolute;
-  top: 10px;
-  left: 10px;
-  background-color: red;
-  width: 10px;
-  height: 10px;
+  top: 0px;
+  margin-left: 5px;
+  background-color: rgba(0,50,100,0.5);
+  border: 1px solid rgba(0,50,100,0.8);
+  border-radius: 5px;
+  width: 5px;
+  height: 5px;
   pointer-events: none;
   z-index: 100;
 `;
